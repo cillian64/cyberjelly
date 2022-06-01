0faa4d37fdcce553bbebf656f2794c8d04ce978f
David Turner <git@dwt27.co.uk>
2022-06-01 21:49:30 +0100

    Blue-green fading hue backdrop

--- code.py
+++ code.py
@@ -3,15 +3,26 @@
 # SPDX-License-Identifier: MIT
 
 import random
-from adafruit_led_animation.animation.rainbowcomet import RainbowComet
-from adafruit_led_animation.helper import PixelMap
-from adafruit_led_animation.group import AnimationGroup
-from neopio import NeoPIO
 import board
+import hsv
+
+from adafruit_led_animation.helper import PixelMap
+from neopio import NeoPIO
+
 
 # Customize for your strands here
 num_strands = 6
 strand_length = 114
+
+fps = 60.0  # approximate measurement
+
+# Pattern constants
+# Time for the backdrop hue to ramp up and down
+backdrop_ramp_time = 30.0 # seconds
+hue_offset_per_strip = 0.15 / num_strands
+backdrop_min = 0.2
+backdrop_max = 0.8 - (hue_offset_per_strip * num_strands)
+
 
 # Make the object to control the pixels
 pixels = NeoPIO(board.GP0, board.GP1, board.GP2, num_strands*strand_length,
@@ -21,21 +32,31 @@ pixels = NeoPIO(board.GP0, board.GP1, board.GP2, num_strands*strand_length,
 strips = [PixelMap(pixels, range(i*strand_length, (i+1)*strand_length), individual_pixels=True)
     for i in range(num_strands)]
 
-# This function makes a comet animation with slightly random settings
-def make_animation(strip):
-    speed = (1+random.random()) * 0.02
-    length = random.randrange(18, 22)
-    bounce = random.random() > .5
-    offset = random.randint(0, 255)
-    return RainbowComet(strip, speed=speed, tail_length=length, bounce=bounce,
-        colorwheel_offset=offset)
+# State for the pattern
+# Ranges between 0.2 and 0.6 going back and forth in a sawtooth
+hue_backdrop = 0.2
+hue_backdrop_increasing = True
 
-# Make an animation for each virtual strip
-animations = [make_animation(strip) for strip in strips]
+def render(strips):
+    global hue_backdrop_increasing
+    global hue_backdrop
 
-# Put the animations into a group so that we can animate them together
-group = AnimationGroup(*animations, )
+    if hue_backdrop_increasing:
+        hue_backdrop += 1.0 / (fps * backdrop_ramp_time)
+        if hue_backdrop >= backdrop_max:
+            hue_backdrop_increasing = False
+    else:
+        hue_backdrop -= 1.0 / (fps * backdrop_ramp_time)
+        if hue_backdrop <= backdrop_min:
+            hue_backdrop_increasing = True
 
-# Show the animations forever
+    for (strip_idx, strip) in enumerate(strips):
+        h = hue_backdrop + strip_idx * hue_offset_per_strip
+        rgb = hsv.hsv_to_rgb(h, 1.0, 1.0)
+        rgb = [int(255 * x) for x in rgb]
+        strip.fill(rgb)
+
 while True:
-    group.animate()
+    render(strips)
+    for strip in strips:
+        strip.show()
