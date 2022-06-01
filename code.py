2813f98cc31dfebe13621e23cf6d3701fb5b96cb
David Turner <git@dwt27.co.uk>
2022-06-01 22:31:23 +0100

    Add ripples

--- code.py
+++ code.py
@@ -13,8 +13,9 @@ from neopio import NeoPIO
 # Customize for your strands here
 num_strands = 6
 strand_length = 114
+num_tendrils = num_strands - 1
 
-fps = 60.0  # approximate measurement
+fps = 30.0  # approximate measurement
 
 # Pattern constants
 # Time for the backdrop hue to ramp up and down
@@ -22,6 +23,8 @@ backdrop_ramp_time = 30.0 # seconds
 hue_offset_per_strip = 0.15 / num_strands
 backdrop_min = 0.2
 backdrop_max = 0.8 - (hue_offset_per_strip * num_strands)
+
+seconds_between_ripples = 3
 
 
 # Make the object to control the pixels
@@ -37,7 +40,13 @@ strips = [PixelMap(pixels, range(i*strand_length, (i+1)*strand_length), individu
 hue_backdrop = 0.2
 hue_backdrop_increasing = True
 
-def render(strips):
+# Ripple location on each tendril, measured in elements
+# (no ripples on the head)
+ripple_locs = [0] * num_tendrils
+ripples_running = False
+frames_since_ripples = 0
+
+def draw_hue_backdrop(strips):
     global hue_backdrop_increasing
     global hue_backdrop
 
@@ -56,7 +65,57 @@ def render(strips):
         rgb = [int(255 * x) for x in rgb]
         strip.fill(rgb)
 
+
+def draw_ripples(strips):
+    global ripple_locs
+    global ripples_running
+    global frames_since_ripples
+
+    num_segs = strand_length // 6
+
+    if ripples_running:
+        ripples_done = [False] * num_tendrils
+        for (strip_idx, strip) in enumerate(strips[:num_tendrils]):
+            ripple_loc = ripple_locs[strip_idx]
+
+            # Only render the ripple of it's within the strand range
+            if ripple_loc < 0:
+                pass
+            elif ripple_loc < num_segs:
+                # Work out colour of less bright trailing seg
+                trail_colour = strip[0]
+                trail_colour = tuple(min(x + 64, 255) for x in trail_colour)
+
+                for i in range(6):
+                    # Brightest segment
+                    strip[ripple_loc * 6 + i] = (255, 255, 255)
+
+                if ripple_loc >= 1:
+                    for i in range(6):
+                        strip[(ripple_loc - 1) * 6 + i] = trail_colour
+                if ripple_loc <= num_segs - 2:
+                    for i in range(6):
+                        strip[(ripple_loc + 1) * 6 + i] = trail_colour
+            else:
+                ripples_done[strip_idx] = True
+
+            ripple_locs[strip_idx] += 1
+
+        if ripples_done == [True] * num_tendrils:
+            # Ripples done for this time
+            ripples_running = False
+            frames_since_ripples = 0
+
+    else:
+        if frames_since_ripples > fps * seconds_between_ripples:
+            # Setup ripples to start again next frame
+            ripples_running = True
+            ripple_locs = [-5, -10, -15, -20, -25]
+        frames_since_ripples += 1
+
+
 while True:
-    render(strips)
+    draw_hue_backdrop(strips)
+    draw_ripples(strips)
     for strip in strips:
         strip.show()
